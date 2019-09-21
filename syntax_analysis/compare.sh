8b8f1ab4f0b371884671cace651942ec74fa67d7
dxadam <adamgrusky@msn.com>
2019-09-21 04:14:09 -0400

    updated compare.sh description

--- syntax_analysis/compare.sh
+++ syntax_analysis/compare.sh
@@ -1,7 +1,27 @@
+# Script:
+#     
+#    compares the output of parser to the output of the reference
+#    parser (sintatico)
+#
+#
+# Usage:
+#
+#    ./compare.sh testinputs_sem/<input-file>
+#
+#
+# Note: 
+#
+#    if there are differences they will be shown as output. No output means
+#    lex generated the same output as lexico.
+
+
 ./sintatico $1 tmpoutput
 ./parser $1 tmpparoutput
 rm paroutput
 rm output
+
+# the project allowed for differences in white space, this cuts it out
 cat tmpparoutput | tr -d " \t\n\r" >> paroutput
 cat tmpoutput | tr -d " \t\n\r" >> output
+
 diff paroutput output
